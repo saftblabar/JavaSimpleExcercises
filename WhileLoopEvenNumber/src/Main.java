5e3e6156d9918d45bfbe2b9de1b56e4aa194bd35
Julia Tomaszewska <rzechonek@gmail.com>
2020-05-15 22:10:08 +0200

    While loop excercise

---
+++ WhileLoopEvenNumber/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    //Display even numbers using method isEven
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public static boolean isEven (int number) {
+        if ((number % 2) == 0) {
+         return true;
+        } else {
+            return false;
+        }
+    }
+}
