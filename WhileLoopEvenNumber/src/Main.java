129008c9f304c782c8d47f32eca79fe9c3b3884c
Julia Tomaszewska <rzechonek@gmail.com>
2020-05-15 22:41:31 +0200

    Displaying only 5 of found numbers

--- WhileLoopEvenNumber/src/Main.java
+++ WhileLoopEvenNumber/src/Main.java
@@ -1,19 +1,27 @@
 public class Main {
 
     //Display even numbers using method isEven
+    // Display only 5 even numbers
 
     public static void main(String[] args) {
-
         int number = 4;
         int finishNumber = 20;
+        int count = 0;
 
         while (number <= finishNumber) {
-           number++;
+            number++;
+
             if (!isEven(number)) {
                 continue;
             }
             System.out.println("Even number: " + number);
+
+            count++;
+            if (count >= 5) {
+                break;
+            }
         }
+        System.out.println("Even numbers found: " + count);
     }
 
     public static boolean isEven(int number) {
