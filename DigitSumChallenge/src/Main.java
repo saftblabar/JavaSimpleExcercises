c78a5ce0cef96ed1dec3729c4bb0a18f60a8e038
Julia Tomaszewska <rzechonek@gmail.com>
2020-05-31 21:01:12 +0200

    Maths excercises in Java

--- DigitSumChallenge/src/Main.java
+++ DigitSumChallenge/src/Main.java
@@ -1,22 +1,90 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(sumDigits(125));
+        //System.out.println(sumDigits(125));
+
+        System.out.println(isPalindrome(1221));
+        //     System.out.println(isPalindrome2(121));
+        System.out.println(sumFirstAndLastDigit(5782));
     }
 
     private static int sumDigits(int number) {
         if (number < 10) {
             return -1;
         }
-            int sum = 0;
+        int sum = 0;
 
         while (number > 0) {
             int digit = number % 10;
-            sum+= digit;
+            sum += digit;
             System.out.println("Number after modulo: " + number);
             number /= 10;
             System.out.println("Number after dividing by 10: " + number);
+            System.out.println(sum);
         }
         return sum;
+    }
+
+    public static boolean isPalindrome(int number) {
+
+        int a = number;
+
+
+        int reversed = 0;
+
+        while (number != 0) {
+
+            reversed = reversed * 10 + number % 10;
+            System.out.println("Reversed: " + reversed);
+
+            number /= 10;
+            System.out.println("Number: " + number);
+        }
+
+
+        return a == reversed;
+
+    }
+
+    public static boolean isPalindrome2(int number) {
+        // Declaring variables
+        int lastDigit;
+        int reverseNumber = 0;
+        int originalNumber = number;
+        // Starting point for checking (reverseNumber = 0)
+        while (number != 0) {
+            lastDigit = number % 10;
+            System.out.println("Extraction of last digit:  " + lastDigit);
+            reverseNumber *= 10;
+            System.out.println("Increasing the value of reverse number " + reverseNumber);
+            reverseNumber += lastDigit;
+            System.out.println("Adding the last digit to reverseNumber: " + reverseNumber);
+            number /= 10;
+            System.out.println("Removing the last digit for the new loop circle: " + number);
+
+        }
+        // Returning true or false depending of result
+        return originalNumber == reverseNumber;
+
+    }
+
+    public static int sumFirstAndLastDigit(int number) {
+
+        if (number < 0) {
+            return -1;
+        }
+
+        int firstdigit = 0;
+        int lastdigit = 0;
+
+        lastdigit = number % 10;
+        firstdigit = number;
+
+        while (firstdigit >= 10) {
+            firstdigit /= 10;
+            System.out.println(firstdigit);
+        }
+
+        return firstdigit + lastdigit;
     }
 }
 
